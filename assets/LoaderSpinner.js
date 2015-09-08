c437551b6fa9ffdedb195208a647c817cfbaf022
Chris Nelson <Chris.Nelson@popagency.com>
2015-09-08 16:38:46 -0700

    v2 updates

--- assets/LoaderSpinner.js
+++ assets/LoaderSpinner.js
@@ -3,41 +3,37 @@
 
 	DESCRIPTION: Universal Ajax loader &amp; spinner
 
-	VERSION: 0.1.0
+	VERSION: 0.2.0
 
 	USAGE: var myLoaderSpinner = new LoaderSpinner('Element', 'Options')
 		@param {jQuery Object}
 		@param {Object}
 
-	AUTHORS: CN
+	AUTHOR: CN
 
 	DEPENDENCIES:
-		- jQuery 1.10+
+		- jQuery 2.1.4+
 
 */
 
 var LoaderSpinner = function($el, objOptions) {
 	this.$el = $el;
 	this.options = $.extend({
-		overlayTemplate: '<div class="overlay"></div>',
-		spinnerTemplate: '<div class="spinner"></div>'
+		overlayTemplate: '<div class="loader-spinner-overlay"></div>'
 	}, objOptions || {});
 	this.$elOverlay = $(this.options.overlayTemplate);
-	this.$elSpinner = $(this.options.spinnerTemplate);
 };
 LoaderSpinner.prototype = {
 	addLoader: function() {
-		this.$el.append(this.$elOverlay, this.$elSpinner);
+		this.$el.append(this.$elOverlay);
 		setTimeout(function() {
-			this.$elSpinner.click(); //spinner gif gets 'stuck' and needs a click
+			this.$elOverlay.click(); //spinner gif gets 'stuck' and needs a click
 		}.bind(this), 10);
 	},
 	removeLoader: function() {
 		this.$elOverlay.remove();
-		this.$elSpinner.remove();
 	}
 };
-
 
 //uncomment to use as a browserify module
 //module.exports = LoaderSpinner;
